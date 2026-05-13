ec1d03a55031bf2038de51f0bf59ed037293477a
Maria <2865701@alu.murciaeduca.es>
2026-05-13 11:12:54 +0200

    Nivel del parque terminado
    Nivel del parque terminado, con una mision que cumplir para poder pasártelo y un objeto secreto el cual solo se puede conseguir si no completas el código antes de ir a por el (Esto posiblemente sea cambiado para permitir ir a por el objeto y pasarte el nivel igualmente, ya sea teletransportandote a la ultima plataforma o poniendo mas).

--- escenas/Niveles/nivel_parque.gd
+++ escenas/Niveles/nivel_parque.gd
@@ -1,9 +1,26 @@
 extends Node2D
 
+@onready var patos: int = 0
+
+@onready var secretoEncontrado: bool = false
 
 func _ready() -> void:
 	$Personaje_Codigo/Primer_Mensaje.show()
 	$Personaje_Codigo/Mensaje_mision.hide()
+	
+	$Plataformas2.hide()
+	$Plataformas2.collision_enabled = false
+	
+	$Plataformas3.hide()
+	$Plataformas3.collision_enabled = false
+	
+	$Plataformas4.hide()
+	$Plataformas4.collision_enabled = false
+
+
+func _process(_delta: float) -> void:
+	$Personaje_Codigo/Estado_Mision.text = str(patos)
+
 
 func _on_puerta_de_salida_body_entered(_body: Node2D) -> void:
 	$Personaje_Codigo/Advertencia.show()
@@ -23,11 +40,56 @@ func _on_muerte_body_entered(_body: Node2D) -> void:
 
 
 func _on_puerta_de_meta_body_entered(_body: Node2D) -> void:
-	# Guardar que el nivel ha sido completado
-	$Personaje_Codigo/Camera2D.enabled = false
-	Cargador.cargar_escena("res://escenas/MapaAldeaEsmeralda/mapa_aldea_esmeralda.tscn", true)
+	# GUARDAR QUE EL NIVEL HA SIDO COMPLETADO Y SI EL COLECCIONABLE SECRETO HA SIDO RECOGIDO
+	if patos == 3:
+		$Personaje_Codigo/Camera2D.enabled = false
+		Cargador.cargar_escena("res://escenas/MapaAldeaEsmeralda/mapa_aldea_esmeralda.tscn", true)
 
 
 func _on_activar_primer_mensaje_body_entered(_body: Node2D) -> void:
 	$Personaje_Codigo/Primer_Mensaje.hide()
 	$Personaje_Codigo/Mensaje_mision.show()
+	
+	$Personaje_Codigo/Fondo_Mision.show()
+	$Personaje_Codigo/Estado_Mision.show()
+	$Personaje_Codigo/Mision.show()
+
+
+func _on_palabra_faltante_body_entered(_body: Node2D) -> void:
+	$PalabraFaltante.hide()
+	$Plataformas1/CodigoErroneo.hide()
+	$Plataformas1/CodigoCorrecto.show()
+	$Plataformas2.show()
+	$Plataformas2.collision_enabled = true
+	$Plataformas4.show()
+	$Plataformas4.collision_enabled = true
+
+
+func _on_pato_1_body_entered(_body: Node2D) -> void:
+	$Pato1.hide()
+	$Pato1/CollisionShape2D.set_deferred("disabled", true)
+	patos += 1
+
+
+func _on_pato_2_body_entered(_body: Node2D) -> void:
+	$Pato2.hide()
+	$Pato2/CollisionShape2D.set_deferred("disabled", true)
+	patos += 1
+
+
+func _on_pato_3_body_entered(_body: Node2D) -> void:
+	$Pato3.hide()
+	$Pato3/CollisionShape2D.set_deferred("disabled", true)
+	patos += 1
+
+
+func _on_palabra_faltante_2_body_entered(_body: Node2D) -> void:
+	$PalabraFaltante2.hide()
+	$Plataformas3.show()
+	$Plataformas3.collision_enabled = true
+
+
+func _on_elemento_secreto_body_entered(_body: Node2D) -> void:
+	$ElementoSecreto.hide()
+	$ElementoSecreto/CollisionShape2D.set_deferred("disabled", true)
+	secretoEncontrado = true
