158c43f1b19417368b4d3763696db8179cc46267
Maria <2865701@alu.murciaeduca.es>
2026-05-24 19:59:46 +0200

    Niveles terminados
    Niveles del banco y biblioteca terminados y advertencia actualizada para que sea menos confusa

--- escenas/Niveles/nivel_banco.gd
+++ escenas/Niveles/nivel_banco.gd
@@ -4,14 +4,22 @@ var interactuando: bool = false
 
 var palabra_tocada = false
 
+var tiempo: String = "00:00"
+var tiempo_total: float = 0.0
+
 func _ready() -> void:
 	$"Personaje_Codigo/Nivel_Tutorial_Mision".hide()
-	$"Personaje_Codigo/Mensaje_Final_Nivel".hide()
 	$Plataforma2.hide()
 	$Palabra_faltante.show()
 	$Plataformas1/Codigo_Incompleto.show()
 	$Plataformas1/Codigo_Completo.hide()
-	$Personaje_Codigo/Primer_Mensaje.show()
+
+func _process(delta: float) -> void:
+	tiempo_total += delta
+	var segundos: int = int(tiempo_total)
+	var milesimas: int = int((tiempo_total - segundos) * 1000)
+	tiempo = "%03d:%03d" % [segundos, milesimas]
+	$Personaje_Codigo/Tiempo.text = tiempo
 
 func _input(_event: InputEvent) -> void:
 	if interactuando and Input.is_action_just_pressed("interactuar"):
@@ -29,20 +37,17 @@ func _on_palabra_faltante_body_entered(_body: Node2D) -> void:
 	$Palabra_faltante.hide()
 
 func _on_activar_primer_mensaje_body_entered(_body: Node2D) -> void:
-	$Personaje_Codigo/Primer_Mensaje.hide()
-	$Personaje_Codigo/Mensaje_Final_Nivel.hide()
 	$Personaje_Codigo/Nivel_Tutorial_Mision.show()
-
-func _on_activar_ultimo_mensaje_body_entered(_body: Node2D) -> void:
-	$Personaje_Codigo/Primer_Mensaje.hide()
-	$Personaje_Codigo/Nivel_Tutorial_Mision.hide()
-	$Personaje_Codigo/Mensaje_Final_Nivel.show()
 
 func _on_muerte_body_entered(_body: Node2D) -> void:
 	# ir al nivel-banco
 	get_tree().reload_current_scene()
 
 func _on_puerta_de_salida_body_entered(_body: Node2D) -> void:
+	$Personaje_Codigo/Advertencia.show()
+
+
+func _on_puerta_de_meta_body_entered(_body: Node2D) -> void:
 	Global.llave_dorada_obtenida = true
 	$AudioStreamPlayer.stop()
 	$AudioStreamPlayer2.play()
@@ -52,3 +57,13 @@ func _on_puerta_de_salida_body_entered(_body: Node2D) -> void:
 	Database.guardar_partida()
 	# ir al mapa
 	Cargador.cargar_escena("uid://c61j2kork7ar5", false)
+
+
+func _on_aceptar_pressed() -> void:
+	$Personaje_Codigo/Camera2D.enabled = false
+	# ir al mapa
+	Cargador.cargar_escena("uid://c61j2kork7ar5", true)
+
+
+func _on_cancelar_pressed() -> void:
+	$Personaje_Codigo/Advertencia.hide()
