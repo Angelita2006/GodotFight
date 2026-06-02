f355d7b90504b09adf2fae7c20065120296663ed
Maria <2865701@alu.murciaeduca.es>
2026-06-02 12:07:05 +0200

    Scripts de pinchos separados

---
+++ escenas/Niveles/nivel-final-pincho2.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+var activado: bool = false
+
+func _process(_delta: float) -> void:
+	if activado:
+		position.y += 12
+
+func _on_activador_pincho_2_body_entered(_body: Node2D) -> void:
+	activado = true
